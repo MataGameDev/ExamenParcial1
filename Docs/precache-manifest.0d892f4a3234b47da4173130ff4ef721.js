9995b4dd0d7bea665fd25e64759b43dc7f6d35e2
unknown <mikes0174@hotmail.com>
2022-04-04 07:49:31 -0500

    pruebas Web

---
+++ Docs/precache-manifest.0d892f4a3234b47da4173130ff4ef721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "69f1f32c8ecefc568365",
+    "url": "/static/css/main.22d41024.chunk.css"
+  },
+  {
+    "revision": "69f1f32c8ecefc568365",
+    "url": "/static/js/main.69f1f32c.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "c4d85cfae300d076fdc0",
+    "url": "/static/js/2.c4d85cfa.chunk.js"
+  },
+  {
+    "revision": "dc951fff484053e1c299",
+    "url": "/static/js/runtime~main.dc951fff.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "069f23aeb410990a265db881b14a9ee7",
+    "url": "/index.html"
+  }
+];
